8670052381b14796fbb0c2479883e964cc86d7d7
Nathan Dias <69260781+ndzin@users.noreply.github.com>
2023-04-22 02:10:49 -0300

    testing..

--- js/fetch.js
+++ js/fetch.js
@@ -1,18 +1,24 @@
 const status = document.getElementById("discord-status");
 const pfp = document.getElementsByClassName("pfp")[0];
-fetch("https://api.lanyard.rest/v1/users/407649282200436738")
-  .then((res) => res.json())
-  .then((af) => {
+axios.get("https://api.lanyard.rest/v1/users/407649282200436738")
+  .then(async (res) => {
     let discordstatus = [];
 
-    af.data.activities.forEach((lonelil) => {
-      let name = lonelil.name || null;
-      let state = lonelil.state || null;
-      let details = af.data.activities[1].details || "no";
-      discordstatus.push(`${name}${state}${details}`);
+    res.data.activities.forEach((stats) => {
+      let name = stats.name || null;
+      let state = stats.state || null;
+      let details = (res.data.activities[1].details) || null;
+
+      if (name != ""){
+        discordstatus.push(name).toString();
+      }
+      if (state != ""){
+        discordstatus.push(state).toString();
+      }
+      if (details != ""){
+        discordstatus.push(details).toString();
+      }
     });
-
-
 
     pfp.style.border = `3px solid #${
       af.data.discord_status == "online"
